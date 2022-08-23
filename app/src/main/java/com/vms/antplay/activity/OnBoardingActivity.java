5b45871e867e63f05ef1374a5dfa252028244731
admin <r.q.abbas@gmail.com>
2022-08-23 14:58:12 +0530

    OnBoarding Screens for tablet are created

--- app/src/main/java/com/vms/antplay/activity/OnBoardingActivity.java
+++ app/src/main/java/com/vms/antplay/activity/OnBoardingActivity.java
@@ -3,7 +3,9 @@ package com.vms.antplay.activity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -19,6 +21,7 @@ public class OnBoardingActivity extends AppCompatActivity implements View.OnClic
     TabLayout tabLayout;
     TextView txtSkip, txtNext;
     int currentItem;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,7 @@ public class OnBoardingActivity extends AppCompatActivity implements View.OnClic
         setContentView(R.layout.activity_on_boarding);
         txtNext = findViewById(R.id.txtNext);
         txtSkip = findViewById(R.id.txtSkip);
+        context = this;
 
         viewPager = findViewById(R.id.viewPager);
         viewPager.setAdapter(new OnBoardingPagerAdapter(getSupportFragmentManager()));
@@ -76,8 +80,9 @@ public class OnBoardingActivity extends AppCompatActivity implements View.OnClic
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.txtSkip:
-                Intent intent = new Intent(OnBoardingActivity.this, MainActivity.class);
-               // startActivity(intent);
+                Intent intent = new Intent(OnBoardingActivity.this, LoginActivity.class);
+                startActivity(intent);
+                updateSharedPreferences(false);
                 // Toast.makeText(OnBoardingActivity.this, "Close This Activity", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.txtNext:
@@ -87,17 +92,27 @@ public class OnBoardingActivity extends AppCompatActivity implements View.OnClic
         }
     }
 
+
     private void movePagerToNextPage() {
         int currentItem = viewPager.getCurrentItem();
         currentItem++;
         Log.d("PAGER", "Current Item: " + currentItem);
         if (currentItem == 6) {
-            Intent intent = new Intent(OnBoardingActivity.this, MainActivity.class);
-            //startActivity(intent);
-
+            Intent intent = new Intent(OnBoardingActivity.this, LoginActivity.class);
+            startActivity(intent);
+            updateSharedPreferences(true);
         } else {
             viewPager.setCurrentItem(currentItem);
         }
 
     }
+
+    private void updateSharedPreferences(boolean isNotFirstTime) {
+
+        SharedPreferences preferences = context.getSharedPreferences("com.vms.antplay",Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean(getString(R.string.is_first_time),isNotFirstTime);
+        editor.apply();
+    }
+
 }
--- app/src/main/java/com/vms/antplay/activity/SplashActivity.java
+++ app/src/main/java/com/vms/antplay/activity/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.vms.antplay.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Handler;
 import android.os.Bundle;
 import android.view.Window;
@@ -9,6 +11,8 @@ import android.view.WindowManager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.vms.antplay.R;
+
+import java.util.prefs.Preferences;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -19,13 +23,24 @@ public class SplashActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
 
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.vms.antplay", Context.MODE_PRIVATE);
+        boolean isNotFirstTime = sharedPreferences.getBoolean(getString(R.string.is_first_time), false);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                // This method will be execute once the timer is over
-                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
-                startActivity(i);
-                finish();
+                if(isNotFirstTime){
+                    // This method will be execute once the timer is over
+                    Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                    startActivity(i);
+                    finish();
+                }else {
+                    // This method will be execute once the timer is over
+                    Intent i = new Intent(SplashActivity.this, OnBoardingActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+
             }
         }, 4000);
     }
