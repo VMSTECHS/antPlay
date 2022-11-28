2cc8e0c2bd625ba3e7d76328e36f1cbe32c61e36
admin <r.q.abbas@gmail.com>
2022-11-28 15:15:23 +0530

    Keystore Created and Updated

--- app/src/main/java/com/vms/antplay/vnc/Constt.java
+++ app/src/main/java/com/vms/antplay/vnc/Constt.java
@@ -3,7 +3,8 @@ package com.vms.antplay.vnc;
 public class Constt {
     //public static String IP_ADDRESS = "103.125.201.180";
     //public static String IP_ADDRESS =   "10.250.51.108";
-    public static String IP_ADDRESS =   "10.250.50.100"; // For Rakesh bhai
+    //public static String IP_ADDRESS =   "10.250.50.101"; // For Qamar
+    public static String IP_ADDRESS =   "10.250.50.100"; // For Rakesh
     public static int PORT = 5900; //5909
     public static String USERNAME = "Antplay-Pc"; //5909
     //  public static String PASSWORD = "test";
